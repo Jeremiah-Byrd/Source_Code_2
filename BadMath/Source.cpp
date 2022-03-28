2b1a47fab3b88d9f2fa80c0634268d58c4358b93
Jeremiah Byrd <jeremiah.byrd@sinclair.edu>
2022-03-27 20:06:40 -0400

    I put parathesis around the average variables to get proper averages. I also added comments to the program and grouped text with inputs. Then I changed the second n2 to n3. And Finally I added an exit message.=)

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -4,20 +4,38 @@ using namespace std;
 
 float average(int i1, int i2, int i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	return (i1 + i2 + i3) / 3.0;
 }
 
 int main()
 {
+	//Variables
 	int n1 =0 , n2 = 0, n3 = 0;
-	cout << "This program calculates the average of three numbers." << endl;
-	cout << "First number: "; cin >> n1;
-	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
 
+	//Introduction Menu
+	cout << "This program calculates the average of three numbers." << endl;
+
+	//Getting the first Number
+	cout << "First number: "; 
+	cin >> n1;
+
+	//Getting the second Number
+	cout << "Second number: ";
+	cin >> n2;
+
+	//Getting the third Number
+	cout << "Third number: ";
+	cin >> n3;
+
+	//Getting the average 
 	float a = average(n1, n2, n3);
 
+	//Couting the average 
 	cout << setprecision(1) << fixed << "The average is " << a << endl;
 
+
+	cin.ignore();
+	"\n\nPress Enter to exit program...";
+	cin.get();
 	return 0;
 }
